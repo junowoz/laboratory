7db4d8ccf8f4c3fb2aa38052a432ecd3be254888
Juan José <junogouvea@gmail.com>
2022-05-01 19:55:59 -0400

    1/05

---
+++ kotlin/Conditionals/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var d20 = 13
+
+    if (d20 >= 17)  {
+        println("The incantation works and the dragon is defeated.")
+    } else {
+        println("The incantation fails and the dragon wins.")
+    }
+}
+
+
