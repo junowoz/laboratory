8d00a92b073c71a48f9c62fec5a50af3671384c1
Juan José <junogouvea@gmail.com>
2022-05-05 07:19:35 -0400

    diy

---
+++ kotlin/DIY_Fucntions/src/main/kotlin/diyfunctions.kt
@@ -0,0 +1,71 @@
+fun getListOfNumbers (): List<Int> {
+
+    var myList = mutableListOf<Int>() //<Int>
+    for (i in 1..7){
+        print("Please enter the $i" + "st number: ")
+        var number = Integer.valueOf(readLine())
+        myList.add(number)
+    }
+    return myList
+}
+
+fun findMax(myList: List<Int>): Int {
+    var largestNumber = myList[0]
+    for (i in myList) {
+        if (i > largestNumber){
+            largestNumber = i
+        }
+    }
+    return largestNumber
+}
+
+fun findMin(myList: List<Int>): Int {
+    var smallestNumber = myList[0]
+    for (i in myList) {
+        if (i < smallestNumber) {
+            smallestNumber = i
+        }
+    }
+    return smallestNumber
+}
+
+fun findAverage(myList: List<Int>): Int{
+    var sum = 0
+    for (i in myList){
+        sum += i
+    }
+    return sum / myList.size
+}
+
+fun checkForValue(myList: List<Int>, numToFind: Int): Boolean {
+    for (i in myList) {
+        if (i == numToFind) {
+            return true
+        }
+    }
+    return false
+}
+
+fun main() {
+    var values = getListOfNumbers()
+    println(values)
+
+    var largestValue = findMax(values)
+    println("The largest number is $largestValue.")
+
+    var smallestValue = findMin(values)
+    println("The smallest number is $smallestValue.")
+
+    var average = findAverage(values)
+    println("The average is $average.")
+
+    println("What number should I check for?")
+    var numToFind = Integer.valueOf(readLine())
+    var containsValue = checkForValue(values, numToFind)
+
+    if (containsValue) {
+        println("$numToFind exists in the list.")
+    } else {
+        println("$numToFind does not exist in the list.")
+    }
+}
