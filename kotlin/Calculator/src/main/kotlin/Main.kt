5716f44ec0cfcc2496dcdd832110e2955644079e
Juan José <junogouvea@gmail.com>
2022-05-06 00:40:42 -0400

    calculator

--- kotlin/Calculator/src/main/kotlin/Main.kt
+++ kotlin/Calculator/src/main/kotlin/Main.kt
@@ -1,7 +1,48 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+class Calculator(val name: String) {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    init {
+        println("$name is the owner of this calculator.")
+    }
+
+    fun add(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+
+    fun substract(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+
+    fun multiply(num1: Int, num2: Int): Int {
+        return num1 * num2
+    }
+
+    fun divide(num1: Int, num2: Int): Any {
+
+        if (num2 != 0) {
+            return num1 / num2
+        } else {
+            return "Error"
+        }
+    }
+
+    fun power(num1: Int, num2: Int): Int {
+        var result = 1
+        for (i in 1..num2){
+            result *= num2
+        }
+
+        return result
+    }
+}
+
+
+
+fun main(){
+
+    var calc = Calculator("Juno")
+    println(calc.add(1,4))
+    println(calc.substract(1,4))
+    println(calc.multiply(1,4))
+    println(calc.divide(1,4))
+    println(calc.power(1,4))
 }
