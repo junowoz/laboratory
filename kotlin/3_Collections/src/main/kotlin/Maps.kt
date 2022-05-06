ab757eac5e045f717f2a29d72ccd30060f6ee416
Juan José <86818341+junowoz@users.noreply.github.com>
2022-05-06 10:47:44 -0300

    Update Maps.kt

--- kotlin/3_Collections/src/main/kotlin/Maps.kt
+++ kotlin/3_Collections/src/main/kotlin/Maps.kt
@@ -1,3 +1,15 @@
-fun maps () {
-
+fun maps() {
+    
+    // Creating an Immutable Map
+    
+    var codeysMap = mapOf("valley" to "scorpions", "creek" to "snakes", "forest" to "bears")
+    println(codeysMap)
+    
+    // Retrieving Keys and Values
+     var uniqueTransport = mapOf("India" to "Tuktuk", "Egypt" to "Felucca", "Peru" to "Totora Boat", "Hong Kong" to "Junk Boat")
+     println("A unique mode of transportation that exists in Peru is ${uniqueTransport["Peru"]}.")
+     println(uniqueTransport.keys)
+     println(uniqueTransport.values)
+    
+    // Creating a Mutable Map
 }
