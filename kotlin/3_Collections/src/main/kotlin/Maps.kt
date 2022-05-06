43622ad66ee68ee633fff353aac541e96d2da55e
Juan José <86818341+junowoz@users.noreply.github.com>
2022-05-06 11:06:23 -0300

    Update Maps.kt

--- kotlin/3_Collections/src/main/kotlin/Maps.kt
+++ kotlin/3_Collections/src/main/kotlin/Maps.kt
@@ -1,7 +1,6 @@
 fun maps() {
     
     // Creating an Immutable Map
-    
     var codeysMap = mapOf("valley" to "scorpions", "creek" to "snakes", "forest" to "bears")
     println(codeysMap)
     
@@ -12,4 +11,20 @@ fun maps() {
      println(uniqueTransport.values)
     
     // Creating a Mutable Map
+    var tvShows = mutableMapOf("The Big Bang Theory" to 278,"Modern Family" to 250, "Bewitched" to 254, "That '70s Show" to 200)
+    tvShows["The Big Bang Theory"] = 279
+    print(tvShows)
+    
+    // Adding and Removing Entries | .put and .remove
+    var momaPaintings = mutableMapOf("Les Demoiselles d'avignon" to "Pablo Picasso", "The Starry Night" to "Vincent Van Gogh", "Mona Lisa" to "Leonardo Da Vinci", "The Persistance of Memory" to "Salvador Dali")
+
+    if (!momaPaintings.containsValue("Claude Monet")) {
+        momaPaintings.put("Water Lillies", "Claude Monet")
+ 
+    }
+
+    momaPaintings.remove("Mona Lisa") // To remove, call the Key (first word)
+    println(momaPaintings)
+  
+
 }
