30d363c78569945aa0792eb5d6209b7486b2b594
junowoz <junogouvea@gmail.com>
2022-12-19 23:38:44 -0500

    java

---
+++ java/1_HelloWorld.java
@@ -0,0 +1,7 @@
+//Writing an Hello World
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    } 
+}
---
+++ kotlin/ASCII_Art/bin/main/Main.kt
@@ -0,0 +1,42 @@
+// Project: ASCII Artist
+// by: @junowoz
+
+fun main() {
+
+    // Box in ASCII
+    /*
+            ________
+            |      |
+            |      |
+            --------
+     */
+    println("________")
+    println("|      |")
+    println("|      |")
+    println("--------")
+
+    //Robot in ASCII
+    /*
+     .....
+  .         .
+ .   0  0    .
+  ...........
+   .. __ ..
+  . .|C_|. .
+ .  ......  .
+     .  .
+     .  .
+    */
+
+    println("     .....")
+    println("  .         .")
+    println(" .   0  0    .")
+    println("  ...........")
+    println("   .. __ ..")
+    println("  . .|C_|. .")
+    println(" .  ......  .")
+    println("     .  .")
+    println("     .  .")
+
+}
+
