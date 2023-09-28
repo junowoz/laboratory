c6caa998ac7b9ac8c37dfe243397b8fa8c97eb9b
junowoz <junogouvea@gmail.com>
2023-09-28 09:30:55 -0400

    lab update java kot + py

--- python/concepts/0_hello_world.py
+++ python/concepts/0_hello_world.py
@@ -1,3 +1,5 @@
+# BASICS
+
 print("Hello, World!")
 # Comments
 variable = "Hi"
@@ -6,4 +8,25 @@ float = 2.0
 string = "2"
 boolean = True
 boolean = False
+exponent =  2**2
+modulo = 29 % 5
 # print(variable)
+
+## Calculations
+# Prints "500"
+print(573 - 74 + 1)
+
+# Prints "50"
+print(25 * 2)
+
+# Prints "2.0"
+print(10 / 5)
+
+## Concatenation
+
+greeting_text = "Hey there!"
+question_text = "How are you doing?"
+full_text = greeting_text + question_text
+
+# Prints "Hey there!How are you doing?"
+print(full_text)
